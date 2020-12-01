263ef78f262ec48ac287af22c876137622bc1568
Jesus A Hernandez de Rojas <jesusangel.hernandezderojas@sky.uk>
2020-12-01 08:26:48 +0000

    feat(): more

--- packages/pack1/src/index.js
+++ packages/pack1/src/index.js
@@ -1,6 +1,8 @@
 // Package content
 const helloWorld = () => console.log('Hello World')
 
+// Comment
+helloWorld()
 helloWorld()
 
 export default helloWorld
--- packages/pack2/src/index.js
+++ packages/pack2/src/index.js
@@ -3,5 +3,7 @@ import helloWorld from '@jhderojas/jhderojas-pack1'
 
 const otherHello = () => console.log('Other Hello World')
 
+// Another comment
+helloWorld()
 helloWorld()
 otherHello()
